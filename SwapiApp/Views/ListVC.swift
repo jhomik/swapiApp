3047802c0b920413bb9c6579cdf0397ac4b5a911
Jakub <jh.homik@gmail.com>
2020-02-18 23:34:28 +0100

    merging branch

--- SwapiApp/Views/ListVC.swift
+++ SwapiApp/Views/ListVC.swift
@@ -11,7 +11,8 @@ import UIKit
 class ListVC: UIViewController {
     
     var tableView = UITableView()
-    var selectedCategory = [PeopleResponse]()
+    var selectedCategory: PeopleResponse?
+    //var selectedCategory = [PeopleResponse]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,12 +36,15 @@ class ListVC: UIViewController {
 extension ListVC: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return selectedCategory.count
+        return selectedCategory?.results.count ?? 0
+        /// There was only one selectedCaregory value, the names are inside the results property
+        // return selectedCategory.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ListVC", for: indexPath)
-        cell.textLabel!.text = selectedCategory[indexPath.row].results[indexPath.row].name
+        cell.textLabel?.text = selectedCategory?.results[indexPath.row].name
+        // cell.textLabel!.text = selectedCategory[indexPath.row].results[indexPath.row].name
         return cell
     }
     
